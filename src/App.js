d150c35b4d83dd8002c48369775b41cec5ee5588
thabares <thabaresnazeer.1995@gmail.com>
2021-04-11 13:31:40 +0530

    added styled components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Header from './Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         <>
           <Switch>
             <Route path='/' exact>
-              This is Home
+              <Header />
             </Route>
           </Switch>
         </>
