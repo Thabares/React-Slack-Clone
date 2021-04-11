2d0c0c59b97a7517363bec638c136704ffdb7a97
thabares <thabaresnazeer.1995@gmail.com>
2021-04-11 13:17:08 +0530

    react router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Build Slack</h1>
+      <Router>
+        <>
+          <Switch>
+            <Route path='/' exact>
+              This is Home
+            </Route>
+          </Switch>
+        </>
+      </Router>
     </div>
   );
 }
