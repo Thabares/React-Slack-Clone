d35da9bf1632a1c524857660bbd8e608fde7a7f2
thabares <thabaresnazeer.1995@gmail.com>
2021-04-11 12:52:36 +0530

    initial setup updated

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAzehfdYmxip1_4cyGz_OcBUjFrAZlKrjs',
+  authDomain: 'slack-clone-40414.firebaseapp.com',
+  projectId: 'slack-clone-40414',
+  storageBucket: 'slack-clone-40414.appspot.com',
+  messagingSenderId: '1004212652633',
+  appId: '1:1004212652633:web:bb536237183869ebcf7f48',
+};
